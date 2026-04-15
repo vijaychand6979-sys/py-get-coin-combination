9191a08061d5c6547f902389dc4f505e7dbc60d3
Vijay Chand <vijaychand6979@googlemail.com>
2026-04-15 11:16:51 +0100

    added test cases

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, expected",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+        (74, [4, 0, 2, 2]),
+        (100, [0, 0, 0, 4])
+    ]
+)
+def test_eval(cents: int, expected: list[int]) -> None:
+    assert get_coin_combination(cents) == expected
